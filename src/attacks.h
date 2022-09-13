6a9a4c2911ba81007dcb45247d7fab4cc94eb86d
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2022-09-13 10:29:10 +0000

    Fix king capture

--- src/attacks.h
+++ src/attacks.h
@@ -16,8 +16,8 @@ constexpr std::array<Map, 4> init_xrays() {
 		for (int i = 0; i < 8; i++) {
 			bb[0] |= NE_move(bb[0]);
 			bb[1] |= NW_move(bb[1]);
-			bb[3] |= SE_move(bb[3]);
-			bb[2] |= SW_move(bb[2]);
+			bb[2] |= SE_move(bb[2]);
+			bb[3] |= SW_move(bb[3]);
 		}
 		for (int i = 0; i < 4; i++) {
 			set_0(bb[i], s);
@@ -34,8 +34,8 @@ constexpr std::array<Map, 4> init_disc_attack() {
 	for (Square s = 0; s < 64; s++) {
 		res[0][s] = NE_move(1ull << s);
 		res[1][s] = NW_move(1ull << s);
-		res[3][s] = SE_move(1ull << s);
-		res[2][s] = SW_move(1ull << s);
+		res[2][s] = SE_move(1ull << s);
+		res[3][s] = SW_move(1ull << s);
 	}
 	for (int i = 0; i < 4; i++)
 		res[i][NONE_SQUARE] = 0;
@@ -47,8 +47,8 @@ constexpr std::array<Map, 4> init_disc_after_attack() {
 	for (Square s = 0; s < 64; s++) {
 		res[0][s] = NE_move(NE_move(1ull << s));
 		res[1][s] = NW_move(NW_move(1ull << s));
-		res[3][s] = SE_move(SE_move(1ull << s));
-		res[2][s] = SW_move(SW_move(1ull << s));
+		res[2][s] = SE_move(SE_move(1ull << s));
+		res[3][s] = SW_move(SW_move(1ull << s));
 	}
 	for (int i = 0; i < 4; i++)
 		res[i][NONE_SQUARE] = 0;
