93889c1bf9ba30e22360c68b5bc4b304f2d5d9b2
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2022-09-26 15:23:01 +0000

    Improve and refactore evaluation

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -18,10 +18,10 @@ Evaluation evaluate(Position const& b) {
 
 inline Evaluation debut_evaluate(Position const& b) {
 	return
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'0008'10ff) - bb_popcount(b.get_discs(BLACK) & 0xff08'1000'0000'0000)) * 12 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'0018'18ff) - bb_popcount(b.get_discs(BLACK) & 0xff18'1800'0000'0000)) * 12 +
 		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'3c00'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'003c'0000'0000)) * 11 +
-		(bb_popcount(b.get_discs(WHITE) & 0xffff'ffc3'c33c'ee00) - bb_popcount(b.get_discs(BLACK) & 0x00f7'2cc3'c3ff'ffff)) * 10 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'003c'00c3'0100) - bb_popcount(b.get_discs(BLACK) & 0x0080'c300'3c00'0000)) * 9  +
+		(bb_popcount(b.get_discs(WHITE) & 0xffff'ffc3'c324'6600) - bb_popcount(b.get_discs(BLACK) & 0x0066'24c3'c3ff'ffff)) * 10 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'003c'00c3'8100) - bb_popcount(b.get_discs(BLACK) & 0x0081'c300'3c00'0000)) * 9  +
 		(bb_popcount(b.get_kings(WHITE)) - bb_popcount(b.get_kings(BLACK))) * 50;
 }
 
@@ -29,8 +29,8 @@ inline Evaluation midlegame_evaluate(Position const& b) {
 	return
 		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'0008'1008) - bb_popcount(b.get_discs(BLACK) & 0x1008'1000'0000'0000)) * 12 +
 		(bb_popcount(b.get_discs(WHITE) & 0xffff'ff3c'0000'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'0000'3cff'ffff)) * 11 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'00c3'fff7'ee77) - bb_popcount(b.get_discs(BLACK) & 0xee77'efff'c300'0000)) * 10 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'0000'0180) - bb_popcount(b.get_discs(BLACK) & 0x0180'0000'0000'0000)) * 9  +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'00c3'fff7'7e7e) - bb_popcount(b.get_discs(BLACK) & 0x7e7e'efff'c300'0000)) * 10 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'0000'8181) - bb_popcount(b.get_discs(BLACK) & 0x8181'0000'0000'0000)) * 9  +
 		(bb_popcount(b.get_kings(WHITE)) - bb_popcount(b.get_kings(BLACK))) * 50;
 }
 
@@ -41,6 +41,6 @@ inline Evaluation endgame_evaluate(Position const& b) {
 		(bb_popcount(b.get_discs(WHITE) & 0x0000'00c3'3c00'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'003c'c300'0000)) * 10 +
 		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'c3ff'ffff) - bb_popcount(b.get_discs(BLACK) & 0xffff'ffc3'0000'0000)) *  9 +
 
-		(bb_popcount(b.get_kings(WHITE) & 0x41a2'5428'142a'4582) - bb_popcount(b.get_kings(BLACK) & 0x41a2'5428'142a'4582)) * 60 +
-		(bb_popcount(b.get_kings(WHITE) & 0xbe5d'abd7'ebd5'ba7d) - bb_popcount(b.get_kings(BLACK) & 0xbe5d'abd7'ebd5'ba7d)) * 50;
+		(bb_popcount(b.get_kings(WHITE) & 0x00a2'5428'142a'4500) - bb_popcount(b.get_kings(BLACK) & 0x00a2'5428'142a'4500)) * 60 +
+		(bb_popcount(b.get_kings(WHITE) & 0xff5d'abd7'ebd5'baff) - bb_popcount(b.get_kings(BLACK) & 0xff5d'abd7'ebd5'baff)) * 50;
 }
