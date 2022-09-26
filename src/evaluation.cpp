75ad118f106899d7455bb13eb46bf69d253ae96b
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2022-09-26 16:15:38 +0000

    Fix evaluation

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -18,29 +18,27 @@ Evaluation evaluate(Position const& b) {
 
 inline Evaluation debut_evaluate(Position const& b) {
 	return
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'0018'18ff) - bb_popcount(b.get_discs(BLACK) & 0xff18'1800'0000'0000)) * 12 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'3c00'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'003c'0000'0000)) * 11 +
-		(bb_popcount(b.get_discs(WHITE) & 0xffff'ffc3'c324'6600) - bb_popcount(b.get_discs(BLACK) & 0x0066'24c3'c3ff'ffff)) * 10 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'003c'00c3'8100) - bb_popcount(b.get_discs(BLACK) & 0x0081'c300'3c00'0000)) * 9  +
-		(bb_popcount(b.get_kings(WHITE)) - bb_popcount(b.get_kings(BLACK))) * 50;
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'0018'18ff) - bb_popcount(b.get_discs(BLACK) & 0xff18'1800'0000'0000)) * 102 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'3c00'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'003c'0000'0000)) * 101 +
+		(bb_popcount(b.get_discs(WHITE) & 0xffff'ffff'c324'e700) - bb_popcount(b.get_discs(BLACK) & 0x00e7'24c3'ffff'ffff)) * 100 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'00c3'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'c300'0000'0000)) *  99 +
+		(bb_popcount(b.get_kings(WHITE)) - bb_popcount(b.get_kings(BLACK))) * 500;
 }
 
 inline Evaluation midlegame_evaluate(Position const& b) {
 	return
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'0008'1008) - bb_popcount(b.get_discs(BLACK) & 0x1008'1000'0000'0000)) * 12 +
-		(bb_popcount(b.get_discs(WHITE) & 0xffff'ff3c'0000'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'0000'3cff'ffff)) * 11 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'00c3'fff7'7e7e) - bb_popcount(b.get_discs(BLACK) & 0x7e7e'efff'c300'0000)) * 10 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'0000'8181) - bb_popcount(b.get_discs(BLACK) & 0x8181'0000'0000'0000)) * 9  +
-		(bb_popcount(b.get_kings(WHITE)) - bb_popcount(b.get_kings(BLACK))) * 50;
+		(bb_popcount(b.get_discs(WHITE) & 0xffff'0000'0018'1818) - bb_popcount(b.get_discs(BLACK) & 0x1818'1800'0000'ffff)) * 102 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'ffff'0000'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'0000'ffff'0000)) * 101 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'ffe7'6666) - bb_popcount(b.get_discs(BLACK) & 0x6666'e7ff'0000'0000)) * 100 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'0000'8181) - bb_popcount(b.get_discs(BLACK) & 0x8181'0000'0000'0000)) *  99 +
+		(bb_popcount(b.get_kings(WHITE)) - bb_popcount(b.get_kings(BLACK))) * 500;
 }
 
 inline Evaluation endgame_evaluate(Position const& b) {
 	return
-		(bb_popcount(b.get_discs(WHITE) & 0xffff'0000'0000'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'0000'0000'ffff)) * 12 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'ff3c'0000'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'0000'3cff'0000)) * 11 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'00c3'3c00'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'003c'c300'0000)) * 10 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'c3ff'ffff) - bb_popcount(b.get_discs(BLACK) & 0xffff'ffc3'0000'0000)) *  9 +
-
-		(bb_popcount(b.get_kings(WHITE) & 0x00a2'5428'142a'4500) - bb_popcount(b.get_kings(BLACK) & 0x00a2'5428'142a'4500)) * 60 +
-		(bb_popcount(b.get_kings(WHITE) & 0xff5d'abd7'ebd5'baff) - bb_popcount(b.get_kings(BLACK) & 0xff5d'abd7'ebd5'baff)) * 50;
+		(bb_popcount(b.get_discs(WHITE) & 0xffff'0000'0000'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'0000'0000'ffff)) * 102 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'ff3c'0000'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'0000'3cff'0000)) * 101 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'00c3'3c00'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'003c'c300'0000)) * 100 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'c3ff'ffff) - bb_popcount(b.get_discs(BLACK) & 0xffff'ffc3'0000'0000)) *  99 +
+		(bb_popcount(b.get_kings(WHITE)) - bb_popcount(b.get_kings(BLACK))) * 500;
 }
