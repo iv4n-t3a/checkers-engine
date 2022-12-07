91177a6e4fac5a7644854cc0b4bbe4568a3d2926
Иван Чай <138519172+iv4n-t3a@users.noreply.github.com>
2022-12-07 22:16:56 +0000

    Improve evaluation

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -9,11 +9,11 @@ Evaluation endgame_evaluate(Position const&);
 Evaluation evaluate(Position const& b) {
 	int count = bb_popcount(b.get_all());
 	if (count >= 18)
-		return debut_evaluate(b);
+	   return debut_evaluate(b);
 	else if (count >= 9)
-		return midlegame_evaluate(b);
-	else
-		return endgame_evaluate(b);
+	   return midlegame_evaluate(b);
+    else
+	   return endgame_evaluate(b);
 }
 
 inline Evaluation debut_evaluate(Position const& b) {
@@ -27,9 +27,9 @@ inline Evaluation debut_evaluate(Position const& b) {
 
 inline Evaluation midlegame_evaluate(Position const& b) {
 	return
-		(bb_popcount(b.get_discs(WHITE) & 0xffff'0000'0018'1818) - bb_popcount(b.get_discs(BLACK) & 0x1818'1800'0000'ffff)) * 102 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'ffff'0000'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'0000'ffff'0000)) * 101 +
-		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'ffe7'6666) - bb_popcount(b.get_discs(BLACK) & 0x6666'e7ff'0000'0000)) * 100 +
+		(bb_popcount(b.get_discs(WHITE) & 0xff7e'0000'0018'1818) - bb_popcount(b.get_discs(BLACK) & 0x1818'1800'0000'7eff)) * 102 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0081'ff7e'0000'0000) - bb_popcount(b.get_discs(BLACK) & 0x0000'0000'7eff'8100)) * 101 +
+		(bb_popcount(b.get_discs(WHITE) & 0x0000'0081'ffe7'6666) - bb_popcount(b.get_discs(BLACK) & 0x6666'e7ff'8100'0000)) * 100 +
 		(bb_popcount(b.get_discs(WHITE) & 0x0000'0000'0000'8181) - bb_popcount(b.get_discs(BLACK) & 0x8181'0000'0000'0000)) *  99 +
 		(bb_popcount(b.get_kings(WHITE)) - bb_popcount(b.get_kings(BLACK))) * 500;
 }
